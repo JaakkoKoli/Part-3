25746aaf42d81660239735999d6c7b0276fbd8ba
8Cookie9 <Cookie3141592653589>
2018-02-10 22:07:31 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -33,10 +33,6 @@ app.use(morgan(':method :url :content :status :res[content-length] - :response-t
 app.use(bodyParser.json())
 app.use(express.static('build'))
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-
 app.get('/info', (req, res) => {
     res.send('puhelinluettelossa on '+persons.length+' henkilön tiedot<br /><br />'+Date())
 })
